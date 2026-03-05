00e5f7b030f8789175f57c298de901adf722030a
ToriTalks2much <x68gh5zdjt@privaterelay.appleid.com>
2026-03-05 09:54:46 -0500

    Implement inventory management functions

---
+++ inventory.py
@@ -0,0 +1,24 @@
+def add_item(inventory, item):
+    inv = inventory()
+    if inv["locked"]:
+        return inv
+    if len(inv["items"]) == inv["capacity"]:
+        raise ValueError
+    if item == "":
+        raise ValueError
+    inv["items"].append(item)
+    return inv
+ 
+def remove_item(inventory, item):
+    inv = inventory()
+    if item not in inv["items"]:
+        raise ValueError
+    if inv["locked"]:
+        return inv
+    inv["items"].remove(item)
+    return inv
+ 
+ 
+def get_item_count(inventory):
+    inv = inventory()
+    return len(inv["items"])
